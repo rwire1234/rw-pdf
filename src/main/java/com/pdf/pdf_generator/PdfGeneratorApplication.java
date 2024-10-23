c9bb78adc65ceb4f2c7a1598e594bf18c5c11c78
NishantRathore-RW <nishant.rathore@researchwire.in>
2024-10-23 11:10:34 +0530

    Removed Unused Imports.

--- src/main/java/com/pdf/pdf_generator/PdfGeneratorApplication.java
+++ src/main/java/com/pdf/pdf_generator/PdfGeneratorApplication.java
@@ -1,15 +1,12 @@
 package com.pdf.pdf_generator;
 
-import java.io.File;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.itextpdf.licensing.base.LicenseKey;
 
 @SpringBootApplication
 public class PdfGeneratorApplication {
 
 	public static void main(String[] args) {
-		LicenseKey.loadLicenseFile(new File("src/main/resources/license.json"));
 		SpringApplication.run(PdfGeneratorApplication.class, args);
 	}
 
